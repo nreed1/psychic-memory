d76afc2a43a10289d6bfc8c587ea6c1f0c103de6
Nicole Reed <nicole.reed88@gmail.com>
2016-08-01 16:18:46 -0400

    Switched over Service URL to HTTPS

--- FieldOutlookAndroid/app/src/main/java/com/example/niki/fieldoutlookandroid/helper/ServiceHelper.java
+++ FieldOutlookAndroid/app/src/main/java/com/example/niki/fieldoutlookandroid/helper/ServiceHelper.java
@@ -8,14 +8,10 @@ public class ServiceHelper {
 
 
     private static final String DEBUG_SERVICE_URI="http://localhost:32251/WebDataService.svc/xml/";
-    private static final String SERVICE_URI="http://fieldoutlookcloudservice.cloudapp.net/WebDataService.svc/xml/";
+    private static final String SERVICE_URI="https://fieldoutlookcloudservice.cloudapp.net:8080/WebDataService.svc/xml/";
 
     public static String GetServiceURL(){
         if(DEBUG==true) {return DEBUG_SERVICE_URI;}
         return SERVICE_URI;
-    }
-
-    public void RefreshData(){
-
     }
 }
